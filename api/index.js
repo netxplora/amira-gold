dec9a4133cad641e764643f2dd0f76ed7fbba75c
netxplora <119927774+netxplora@users.noreply.github.com>
2026-05-13 22:05:33 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import server from '../dist/server/server.js';
 
+export const config = {
+  runtime: 'nodejs',
+};
 
 export default async function handler(request) {
   return await server.fetch(request);
